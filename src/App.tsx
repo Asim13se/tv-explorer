5874bed75bf76b0e65e9431f6b33024ed052a5e1
asim <asim13se@gmail.com>
2023-06-22 13:48:30 +0300

    movile list component implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import FilmSeriesExplorer from "./components/app/FilmSeriesExplorer/FilmSeriesExplorer";
+import getStyle from "./utils/getStyle";
+import data from "./assets/data.json";
 
 function App() {
   return (
     <div className="App">
-
+      <FilmSeriesExplorer
+        filmSeries={data.filmSeriesList[0]}
+        style={styles.filmSeriesExplorer}
+      />
     </div>
   );
 }
+
+const styles = getStyle({
+  filmSeriesExplorer: {
+    marginTop: 50,
+    marginLeft: 30,
+    marginInlineStart: 30,
+  }
+})
 
 export default App;
